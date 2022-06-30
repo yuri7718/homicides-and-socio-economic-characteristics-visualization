1dbb9d4ba2412d392f85fc0e3bc50eb842b03911
yuri7718 <qiangxu1204@gmail.com>
2022-06-29 17:42:17 -0700

    clean heat map

--- src/heatmap/Heatmap.js
+++ src/heatmap/Heatmap.js
@@ -1,194 +1,134 @@
 import React from 'react';
 import * as d3 from 'd3';
-//import { getColorScale, getExtrema, showMap, hideMap } from './helper';
 import { getAverage, getTooltipText } from './helper';
 
 class Heatmap extends React.Component {
-    constructor(props) {
-        super(props);
-        this.canvasRef = React.createRef(); 
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef(); 
 
-        // map data
-        this.yearRange = [];
-        this.stateGeojson = [];
-        this.countyGeojson = [];
+    this.tooltip = d3.select('body')
+      .append('div')
+      .style('position', 'absolute')
+      .style('padding', '15px')
+      .style('visibility', 'hidden')
+      .style('background-color', 'black')
+      .style('opacity', 0.7)
+      .style('color', '#fff')
+      .style('left', '0px')
+      .style('top', '0px');
+  }
 
-        // map id
-        this.STATE_MAP_ID = 'h-state';
-        this.COUNTY_MAP_ID = 'h-county';
+  
+  drawHeatMap(data) {
 
-        // number of colors for the color scale
-        this.STATE_COLORS = 7;
-        this.COUNTY_COLORS = 7;
+    const { scrollWidth, scrollHeight } = this.canvasRef.current;
+    const margins = { top: 30, right: 30, bottom: 100, left: 200 };
+    const width = scrollWidth - margins.left - margins.right;
+    const height = scrollHeight - margins.top - margins.bottom;
 
-        this.featureList = {};
+    // process dataset so that it can be used by the heat map
+    var cleanedData = []
+    this.props.featureList.forEach(feature => {
+      this.props.years.forEach(year => {
+        let avg = getAverage(data, feature.key, year);
+        cleanedData.push({ feature: feature.feature, year: '19' + year, value: avg });
+      })
+    });
 
-        this.features = this.props.featureList;
-        
-        this.tooltip = d3.select('body')
-        .append('div')
-        .attr('class', 'tooltip')
-        .style('position', 'absolute')
-        .style('padding', '15px')
-        .style('visibility', 'hidden')
-        .style('background-color', 'black')
-        .style('opacity', 0.7)
-        .style('color', '#fff')
-        .style('left', '0px')
-        .style('top', '0px');
-    }
-
-
-
-
-    drawHeatMap(data) {
-       
-        const { scrollWidth, scrollHeight } = this.canvasRef.current;
-        const margins = { top: 30, right: 30, bottom: 100, left: 200 };
-        const width = scrollWidth - margins.left - margins.right;
-        const height = scrollHeight - margins.top - margins.bottom;
-
-        //console.log(this.props.countyCSV);
-        //console.log(this.props.featureList)
-
-        if (this.props.currentState !== '') {
-            // a state is selected
-            // filter counties that are in the state
-            data = data.filter(d => d.STATE_NAME === this.props.currentState);
-        }
-        //console.log(data);
-        var cleanedData = []
-        this.props.featureList.forEach(feature => {
-            this.props.years.forEach(year => {
-                let avg = getAverage(data, feature.key, year);
-                
-                cleanedData.push({ feature: feature.feature, year: '19' + year, value: avg });
-            })
-        });
-
-        const xScale = d3.scaleBand()
-            .range([0, width])
-            .domain(this.props.years.map(year => '19' + year));
-        
-        const yScale = d3.scaleBand()
-            .range([0, height])
-            .domain(this.props.featureList.map(feature => feature.feature))
-            
-
-        //Setting chart width and adjusting for margins
-        const svg = d3.select(this.canvasRef.current).select('svg')
-            .attr('width', scrollWidth)
-            .attr('height', scrollHeight);
-
-        const rootGroup = svg.select('g#root')
-            .attr('transform', `translate(${margins.left}, ${margins.top})`);
-        
-
-        
-        //Append x axis
-        rootGroup.append('g')
-            .call(d3.axisBottom(xScale).ticks(4))
-            .attr('transform', `translate(0, ${height})`) 
-
-        //Append y axis
-        const yAxis = rootGroup.append('g')
-            .call(d3.axisLeft(yScale))
-            //.attr('transform', `translate(${width}, 0)`)
-
-        const dataPerFeature = {};
-        cleanedData.forEach(d => {
-            if (dataPerFeature.hasOwnProperty(d.feature)) {
-                dataPerFeature[d.feature].push(d.value);
-            } else {
-                dataPerFeature[d.feature] = [d.value];
-            }
-        })
-        
-        const colorScales = {};
-        for (let key in dataPerFeature) {
-            colorScales[key] = d3.scaleLinear()
-                .range(['#eff3ff', '#08519c'])
-                .domain(d3.extent(dataPerFeature[key]));
-        }
-        /*
-        const colorScale = d3.scaleLinear()
-            .range(['#eff3ff', '#08519c'])
-            .domain(d3.extent(cleanedData.map(d => d.value)));
-        */
-        const region = this.props.currentState;
-        rootGroup.selectAll()
-            .data(cleanedData)
-            .enter()
-            .append('rect')
-            .attr('x', d => xScale(d.year))
-            .attr('y', d => yScale(d.feature))
-            .attr('width', xScale.bandwidth())
-            .attr('height', yScale.bandwidth())
-            .style('fill', d => colorScales[d.feature](d.value))
-            .on('mouseover', (event, d) => {
-                this.tooltip.style('visibility', 'visible');
-                this.tooltip.html(getTooltipText(this.props.currentState, d))
-                  .style('left', (event.pageX + 10) + 'px')
-                  .style('top', (event.pageY + 10) + 'px');
-              })
-              .on('mouseout', () => {
-                this.tooltip.style('visibility', 'hidden')
-              });
-
-        yAxis.selectAll('g').selectAll('text')
-            .attr('font-weight', d => {
-                return d === 'Homicide rate' ? 700 : 400
-            }).attr('font-size', d => {
-                return d === 'Homicide rate' ? '20px' : '10px'
-            })
-        
-        
-            
-    }
-
-    clearMap() {
-        d3.select(this.canvasRef.current).select('#root').selectAll('*').remove();
-    }
- 
-    /*
-    drawCounties(svg, colorScale) {
-        // console.log(" ----> colorScale = ", colorScale)
-        
-        svg.append('g')
-            .attr('id', this.COUNTY_MAP_ID)
-            .append('rect')
-            .data(this.countyGeojson)
-            .enter()
-            .attr('fill', d => colorScale(d.properties[this.state.property]))
-            .style('stroke', '#000')
+    const xScale = d3.scaleBand()
+      .range([0, width])
+      .domain(this.props.years.map(year => '19' + year));
     
-    }*/
+    const yScale = d3.scaleBand()
+      .range([0, height])
+      .domain(this.props.featureList.map(feature => feature.feature))
+            
+    const svg = d3.select(this.canvasRef.current).select('svg')
+      .attr('width', scrollWidth)
+      .attr('height', scrollHeight);
 
-    componentDidMount() {
-        
-        this.props.featureList.forEach(element => {
-            this.featureList[element.key] = element.feature;
-        });
-       
-          
+    const rootGroup = svg.select('g#root')
+      .attr('transform', `translate(${margins.left}, ${margins.top})`);
+    
+    // append x-axis
+    rootGroup.append('g')
+      .call(d3.axisBottom(xScale).ticks(this.props.years.length))
+      .attr('transform', `translate(0, ${height})`) 
+
+    // append y-axis
+    const yAxis = rootGroup.append('g')
+      .call(d3.axisLeft(yScale))
+
+    // reorganize the data to create color scale per feature
+    const dataPerFeature = {};
+    cleanedData.forEach(d => {
+      if (dataPerFeature.hasOwnProperty(d.feature)) {
+        dataPerFeature[d.feature].push(d.value);
+      } else {
+        dataPerFeature[d.feature] = [d.value];
+      }
+    });
+    
+    const colorScales = {};
+    for (let feature in dataPerFeature) {
+      colorScales[feature] = d3.scaleLinear()
+        .range(['#eff3ff', '#08519c'])
+        .domain(d3.extent(dataPerFeature[feature]));
     }
 
-    render() {
-        if (this.props.countyCSV.length > 0) {
-            this.clearMap();
-            this.drawHeatMap(this.props.countyCSV);
-        }
-        const text = this.props.currentState === '' ? ('Socio-economic characteristics in the US') :
-            'Socio-economic characteristics in ' + this.props.currentState;
-        return (
-            <div style={{ height: '100%' }} ref={this.canvasRef}>
-                <div style={{textAlign: 'center'}}>{text}</div>
-                <svg style={{ width: '100%', height: '100%' }}>
-                    <g id='root'></g>
-                </svg>
-            </div>
-        );
+    rootGroup.selectAll()
+      .data(cleanedData)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.year))
+      .attr('y', d => yScale(d.feature))
+      .attr('width', xScale.bandwidth())
+      .attr('height', yScale.bandwidth())
+      .style('fill', d => colorScales[d.feature](d.value))
+      .on('mouseover', (event, d) => {
+        this.tooltip.style('visibility', 'visible');
+        this.tooltip.html(getTooltipText(this.props.currentState, d))
+          .style('left', (event.pageX - 200) + 'px')
+          .style('top', (event.pageY + 10) + 'px');
+      }).on('mouseout', () => {
+        this.tooltip.style('visibility', 'hidden')
+      });
+
+    // increase font weight and font size for homicide rate as it is the main feature
+    yAxis.selectAll('g').selectAll('text')
+      .attr('font-weight', d => d === 'Homicide rate' ? 700 : 400)
+      .attr('font-size', d => d === 'Homicide rate' ? '20px' : '10px');
+  }
+
+  clearMap() {
+    d3.select(this.canvasRef.current).select('#root').selectAll('*').remove();
+  }
+ 
+  render() {
+    if (this.props.countyCSV.length > 0) {
+
+      // filter the dataset if a state is selected
+      const data = this.props.currentState === '' ?
+        this.props.countyCSV :
+        this.props.countyCSV.filter(d => d.STATE_NAME === this.props.currentState);
+      
+      this.clearMap();
+      this.drawHeatMap(data);
     }
+    // create title based on selected state
+    const title = this.props.currentState === '' ?
+      'Socio-economic Characteristics in the US' :
+      'Socio-economic Characteristics in ' + this.props.currentState;
+    return (
+      <div style={{ height: '100%' }} ref={this.canvasRef}>
+        <div style={{textAlign: 'center'}}>{title}</div>
+          <svg style={{ width: '100%', height: '100%' }}>
+            <g id='root'></g>
+          </svg>
+      </div>
+    );
+  }
 }
 
 export default Heatmap;
--- src/heatmap/helper.js
+++ src/heatmap/helper.js
@@ -1,32 +1,35 @@
 
-
+/**
+ * Calculate the average value for filtered or non-filtered dataset based on input feature and year
+ */
 export function getAverage(data, feature, year) {
-
   const property = feature + year;
   let sum = 0;
-  data.forEach(d => sum += Number(d[property]));
+  data.forEach(d => sum += d[property]);
   return sum / data.length;
 }
 
+/**
+ * Create tooltip text for heat map
+ */
 export function getTooltipText(currentState, d) {
   var html = ''
   if (currentState === '') {
     html = 
       `<div>
-        <p>${d.feature}</p>
-        <p>Region: The whole country</p>
-        <p>Year ${d.year}</p>
-        <p>Value: ${d.value.toFixed(2)}</p>
+        <p style="text-align: center"><b>${d.feature}</b></p>
+        <p><b>Region</b>: The Whole Country</p>
+        <p><b>Year</b>: ${d.year}</p>
+        <p><b>Value</b>: ${d.value.toFixed(2)}</p>
       </div>`
   } else {
     html = 
       `<div>
-        <p>${d.feature}</p>
-        <p>Region: State ${currentState}</p>
-        <p>Year ${d.year}</p>
-        <p>Value: ${d.value.toFixed(2)}</p>
+        <p style="text-align: center"><b>${d.feature}</b></p>
+        <p><b>Region</b>: ${currentState} State</p>
+        <p><b>Year</b>: ${d.year}</p>
+        <p><b>Value</b>: ${d.value.toFixed(2)}</p>
       </div>`
   }
-  console.log(html);
   return html;
 }
